303b576ed2f3cf02e4b3685c845c75c530ea82fa
cniimf_ot <ot@cniimf.ru>
2024-02-21 07:12:49 +0300

    add tests for hw-2

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,16 +1,34 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
-
 from src.item import Item
 
-test = Item("Смартфон", 10000, 20)
+data = Item("Смартфон", 10000, 20)
 
-def test_calculate_total_price():
-    """Проверяет правильность выполнения метода, который подсчитывает total price"""
-    assert test.calculate_total_price() == 200000
-    assert test.quantity == 20
 
-def test_apply_discount():
-    """Проверяет правильность выполнения метода, который подсчитвает установленную скидку для конкретного товара"""
-    test.pay_rate = 0.8
-    test.apply_discount()
-    assert test.price == 8000.0
+class Tests:
+    def test_calculate_total_price(self):
+        """Проверяет правильность выполнения метода, который подсчитывает total price"""
+        assert data.calculate_total_price() == 200000
+        assert data.quantity == 20
+
+    def test_apply_discount(self):
+        """Проверяет правильность выполнения метода, который подсчитвает установленную скидку для конкретного товара"""
+        data.pay_rate = 0.8
+        data.apply_discount()
+        assert data.price == 8000.0
+
+    def test_name(self):
+        """Тест сеттера сокращающего длину имени до 10(и) символов."""
+        data.name = "Abrakadabra"
+        assert len(data.name) == 10
+
+    def test_instantiate_from_csv(self):
+        """
+        Проверка метода инициализируеющнго экземпляры класса Item данными из файла src/items.csv
+        """
+        Item.instantiate_from_csv('src/items.csv')
+        assert len(Item.all) == 5
+
+    def test_string_to_number(self):
+        """
+        Проверка метода преоброзвания строки в число
+        """
+        assert data.string_to_number("200") == 200
